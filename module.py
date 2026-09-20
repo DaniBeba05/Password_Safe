4408ad45d96418b6bcbad39e4a61db66c6fcfde0
Danibeba <danibeba05ingenieroinformatico@gmail.com>
2026-09-20 11:32:00 +0200

    Modificación del archivo module.py para encerrar las funciones dentro de una clase

--- module.py
+++ module.py
@@ -1,92 +1,92 @@
-def tiene_longitud_maxima (contraseña, minimo = 12 ):
-    longitud_maxima = False
-    if len(contraseña) >= minimo: 
-        longitud_maxima = True
 
-    return longitud_maxima
+class Password: 
+
+    def __init__(self, contraseña, num_intentos, intentos_maximos = 3, minimo = 12):
+        self.contraseña = contraseña
+        self.num_intentos = num_intentos
+        self.intentos_maximos = intentos_maximos
+        self.minimo = minimo
 
 
-
-def tiene_una_mayuscula (contraseña):
+    def introducirpassword(self):
     
-    return any(caracter.isupper() for caracter in contraseña)
+        self.contraseña = str (input("Password:"))
+    
+        return self.contraseña
 
+    
+    def tiene_longitud_maxima (self):
 
+        return len (self.contraseña) >= self.minimo
 
-def tiene_un_numero (contraseña):
+    
+
+    def tiene_una_mayuscula (self):
+    
+        return any(caracter.isupper() for caracter in self.contraseña)
+
+    
+
+    def tiene_un_numero (self):
    
-    return any (caracter.isdigit() for caracter in contraseña)
+        return any (caracter.isdigit() for caracter in self.contraseña)
 
 
+    def tiene_un_simbolo (self):
+
+        simbolos = '.,@#?!>;:-_'
+
+        return any(caracter in simbolos for caracter in self.contraseña)
+
+    
+
+    def comprobar_num_intentos(self):
+
+        return self.num_intentos < self.intentos_maximos
 
 
-def tiene_un_simbolo (contraseña):
+    def sumar_num_intentos (self):
 
-    simbolos = '.,@#?!>;:-_'
-
-    return any(caracter in simbolos for caracter in contraseña)
+        self.num_intentos+=1
 
 
+    def comprobar_password_valida(self):
 
+        return (self.tiene_longitud_maxima() and self.tiene_una_mayuscula() and self.tiene_un_numero() and self.tiene_un_simbolo())
 
-def comprobar_num_intentos(num_intentos, intentos_maximos):
-
-    return num_intentos < intentos_maximos
-
-
-
-def sumar_num_intentos (num_intentos):
-
-    return num_intentos + 1
-
-
-
-def comprobar_password_valida(password):
-
- return (tiene_longitud_maxima(password) and tiene_una_mayuscula(password) and tiene_un_numero(password) and tiene_un_simbolo(password))
-
-
-def mensajedepasswordvalida():
+    def mensajedepasswordvalida(self):
          
-    return "La contraseña introducida es valida"
+        return "La contraseña introducida es valida"
+
+    
+    def mensajedesuperaciondeintentos(self):
+
+        return "Has superado el número de intentos posibles. Vuelve a intentarlo más tarde"
 
 
 
+    def mensajerequisitomayuscula(self):
 
-def mensajedesuperaciondeintentos():
-
-    return "Has superado el número de intentos posibles. Vuelve a intentarlo más tarde"
-
+        return "Compruebe si su contraseña cumple los requisitos de tener al menos una mayúscula"
 
 
 
-def mensajerequisitomayuscula():
+    def mensajerequisitonumero(self):
 
-    return "Compruebe si su contraseña cumple los requisitos de tener al menos una mayúscula"
-
-
-
-def mensajerequisitonumero():
-
-    return "Compruebe si su contraseña cumple los requisitos de tener al menos un número"
+        return "Compruebe si su contraseña cumple los requisitos de tener al menos un número"
 
 
 
-def mensajerequisitosimbolo():
+    def mensajerequisitosimbolo(self):
 
-    return "Compruebe si su contraseña cumple los requisitos de tener al menos un simbolo"
-
-
-
-def mensajerequisitonumcaracteres():
-
-    return "Compruebe si su contraseña cumple los requisitos de tener al menos 12 carácteres"
+        return "Compruebe si su contraseña cumple los requisitos de tener al menos un simbolo"
 
 
 
-def introducirpassword():
+    def mensajerequisitonumcaracteres(self):
 
-    password = str (input("Password:"))
+        return "Compruebe si su contraseña cumple los requisitos de tener al menos 12 carácteres"
 
-    return password
 
+
+   
